4b2c2c06b9d6ad3bdb58fd60d28d779876a7111d
nichoth <nichoth@gmail.com>
2018-11-13 19:46:18 -0800

    stuff

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Start the server
 
     npm start
 
+
+https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-console.log('hello world')
-
 var worker = new window.Worker('worker.js')
 worker.onmessage = function (ev) {
     console.log('Main thread: got message from worker', ev)
     console.log(ev.data)
+}
+
+worker.onerror = function (err) {
+    console.log('error', err)
 }
 
 var form = document.getElementById('demo-form')
